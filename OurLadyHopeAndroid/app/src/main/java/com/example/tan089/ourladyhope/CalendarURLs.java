4ea06ccc847827e00867b0968123e9db3761f057
tando89 <tantdo89@gmail.com>
2018-03-28 13:30:53 -0700

    urls for images updated

--- OurLadyHopeAndroid/app/src/main/java/com/example/tan089/ourladyhope/CalendarURLs.java
+++ OurLadyHopeAndroid/app/src/main/java/com/example/tan089/ourladyhope/CalendarURLs.java
@@ -2,18 +2,18 @@ package com.example.tan089.ourladyhope;
 
 public class CalendarURLs {
     //Calendars images urls
-    public static final String IMPORTANT_DATES= "https://drive.google.com/uc?id=1vqgATWX7_uqMCHMjT3qoXOt2924Gfz9m";
-    public static final String JAN = "https://drive.google.com/uc?id=1eb3cwrEessasJTQGxt7kypdQMK-_IZJ4";
-    public static final String FEB = "https://drive.google.com/uc?id=172HETdyGkMUVqN-QPvj-D192GHg2vqwj";
-    public static final String MAR = "https://drive.google.com/uc?id=14LpfhGsOt93XLOCkvoA1Nhf6BHahX4cL";
-    public static final String APR = "https://drive.google.com/uc?id=1OiA7KMru5ZkLEe3c1Myv8HXA9GR8rDqM";
-    public static final String MAY = "https://drive.google.com/uc?id=1tvPf2InLwUMIyxyeMP7KhoT-R8EKaTHm";
-    public static final String JUN = "https://drive.google.com/uc?id=1luHd6aUUjaWcYQHu18c_gEoVz4a6Cnb-";
-    public static final String JUL = "https://drive.google.com/uc?id=1FxF9EtnE0RJ-SnE4rEjXGp6FLroqafBX";
-    public static final String AUG = "https://drive.google.com/uc?id=116uW3j9mqWq8IQdJ0ZKkmljKxuHGUKdv";
-    public static final String SEP ="https://drive.google.com/uc?id=1AWOS8gtbVqWirkgul-4hytLhNuHvM-J3";
-    public static final String OTC = "https://drive.google.com/uc?id=1sm_AGsMcRJroIdC0Kz1QDDZ-tnOrfL85";
-    public static final String NOV = "https://drive.google.com/uc?id=15jFOLVBTe1TAD8AlMM35Tiky1tpj8hWa";
-    public static final String DEC = "https://drive.google.com/uc?id=1MOWKodoiVN8TYxTgwBuOvVb1_j-36wvR";
+    public static final String IMPORTANT_DATES= "https://raw.githubusercontent.com/tando89/myImagesLinks/master/frontcalendar.png";
+    public static final String JAN = "https://raw.githubusercontent.com/tando89/myImagesLinks/master/jan.png";
+    public static final String FEB = "https://raw.githubusercontent.com/tando89/myImagesLinks/master/feb.png";
+    public static final String MAR = "https://raw.githubusercontent.com/tando89/myImagesLinks/master/march.png";
+    public static final String APR = "https://raw.githubusercontent.com/tando89/myImagesLinks/master/april.png";
+    public static final String MAY = "https://raw.githubusercontent.com/tando89/myImagesLinks/master/may.png";
+    public static final String JUN = "https://raw.githubusercontent.com/tando89/myImagesLinks/master/june.png";
+    public static final String JUL = "https://raw.githubusercontent.com/tando89/myImagesLinks/master/july.png";
+    public static final String AUG = "https://raw.githubusercontent.com/tando89/myImagesLinks/master/august.png";
+    public static final String SEP = "https://raw.githubusercontent.com/tando89/myImagesLinks/master/sep.png";
+    public static final String OTC = "https://raw.githubusercontent.com/tando89/myImagesLinks/master/oct.png";
+    public static final String NOV = "https://raw.githubusercontent.com/tando89/myImagesLinks/master/nov.png";
+    public static final String DEC = "https://raw.githubusercontent.com/tando89/myImagesLinks/master/dec.png";
 
 }
