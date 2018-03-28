03023615487cba408d1eab8f45c8434c65a19411
tando89 <tantdo89@gmail.com>
2018-03-28 00:07:16 -0700

    finshed calendar page. Using piscasso to grab images from Google Drive. Created CalendarURLs class to store the urls.

--- OurLadyHopeAndroid/app/src/main/java/com/example/tan089/ourladyhope/Calendar.java
+++ OurLadyHopeAndroid/app/src/main/java/com/example/tan089/ourladyhope/Calendar.java
@@ -7,14 +7,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class Calendar extends AppCompatActivity {
+import com.squareup.picasso.Picasso;
 
+public class Calendar extends AppCompatActivity {
+    private ImageView img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, imgChristmas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        img1 = (ImageView) findViewById(R.id.img1);
+        img2 = (ImageView) findViewById(R.id.img2);
+        img3 = (ImageView) findViewById(R.id.img3);
+        img4 = (ImageView) findViewById(R.id.img4);
+        img5 = (ImageView) findViewById(R.id.img5);
+        img6 = (ImageView) findViewById(R.id.img6);
+        img7 = (ImageView) findViewById(R.id.img7);
+        img8 = (ImageView) findViewById(R.id.img8);
+        img9 = (ImageView) findViewById(R.id.img9);
+        img10 = (ImageView) findViewById(R.id.img10);
+        img11 = (ImageView) findViewById(R.id.img11);
+        img12 = (ImageView) findViewById(R.id.img12);
+        imgChristmas = (ImageView) findViewById(R.id.imgChristmas);
+
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
         BottomNavigationHelper.disableShiftMode(bottomNavigationView);
         Menu menu = bottomNavigationView.getMenu();
@@ -47,5 +64,27 @@ public class Calendar extends AppCompatActivity {
             }
 
         });
+
+        loadImages(CalendarURLs.IMPORTANT_DATES, img1);
+        loadImages(CalendarURLs.JAN, img2);
+        loadImages(CalendarURLs.FEB, img3);
+        loadImages(CalendarURLs.MAR, img4);
+        loadImages(CalendarURLs.APR, img5);
+        loadImages(CalendarURLs.MAY, img6);
+        loadImages(CalendarURLs.JUN, img7);
+        loadImages(CalendarURLs.JUL, img8);
+        loadImages(CalendarURLs.AUG, img9);
+        loadImages(CalendarURLs.SEP, img10);
+        loadImages(CalendarURLs.OTC, img11);
+        loadImages(CalendarURLs.NOV, img12);
+        loadImages(CalendarURLs.DEC, imgChristmas);
+
+    }
+
+    private ImageView loadImages(String url, ImageView image) {
+        Picasso.with(getApplicationContext())
+                .load(url)
+                .into(image);
+        return image;
     }
 }
---
+++ OurLadyHopeAndroid/app/src/main/java/com/example/tan089/ourladyhope/CalendarURLs.java
@@ -0,0 +1,19 @@
+package com.example.tan089.ourladyhope;
+
+public class CalendarURLs {
+    //Calendars images urls
+    public static final String IMPORTANT_DATES= "https://drive.google.com/uc?id=1vqgATWX7_uqMCHMjT3qoXOt2924Gfz9m";
+    public static final String JAN = "https://drive.google.com/uc?id=1eb3cwrEessasJTQGxt7kypdQMK-_IZJ4";
+    public static final String FEB = "https://drive.google.com/uc?id=172HETdyGkMUVqN-QPvj-D192GHg2vqwj";
+    public static final String MAR = "https://drive.google.com/uc?id=14LpfhGsOt93XLOCkvoA1Nhf6BHahX4cL";
+    public static final String APR = "https://drive.google.com/uc?id=1OiA7KMru5ZkLEe3c1Myv8HXA9GR8rDqM";
+    public static final String MAY = "https://drive.google.com/uc?id=1tvPf2InLwUMIyxyeMP7KhoT-R8EKaTHm";
+    public static final String JUN = "https://drive.google.com/uc?id=1luHd6aUUjaWcYQHu18c_gEoVz4a6Cnb-";
+    public static final String JUL = "https://drive.google.com/uc?id=1FxF9EtnE0RJ-SnE4rEjXGp6FLroqafBX";
+    public static final String AUG = "https://drive.google.com/uc?id=116uW3j9mqWq8IQdJ0ZKkmljKxuHGUKdv";
+    public static final String SEP ="https://drive.google.com/uc?id=1AWOS8gtbVqWirkgul-4hytLhNuHvM-J3";
+    public static final String OTC = "https://drive.google.com/uc?id=1sm_AGsMcRJroIdC0Kz1QDDZ-tnOrfL85";
+    public static final String NOV = "https://drive.google.com/uc?id=15jFOLVBTe1TAD8AlMM35Tiky1tpj8hWa";
+    public static final String DEC = "https://drive.google.com/uc?id=1MOWKodoiVN8TYxTgwBuOvVb1_j-36wvR";
+
+}
